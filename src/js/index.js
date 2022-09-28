a5fd0246cc4090b56d9c5c08d203e1363dffbc57
Gabriele-Stringano <gabrielestringano@gmail.com>
2022-09-29 01:06:23 +0200

    Development update
    - updated style sheets
    -introduced first (test) event for button

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-import '../css/stile.css';
+import '../css/mobile.css';
+import '../css/desktop.css';
+
+let searchBtn= document.getElementById('search');
+let subjectsInput = document.getElementById('subjects');
+
+searchBtn.addEventListener('click', () => {
+  console.log("tutto ok");
+});
